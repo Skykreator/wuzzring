7d29667493e23767f85f9057701dd79286174846
Miles <suntop.son@gmail.com>
2026-09-18 12:40:50 -0400

    favicon

--- directory.php
+++ directory.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <link rel="icon" href="../favicon.ico">
+        <link rel="icon" href="favicon.ico">
         <title>WuzzRing</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
